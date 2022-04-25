a4a1cb2a194d2ac000fd2fc12ca606fd093b7fd5
blairfelts <99297152+blairfelts@users.noreply.github.com>
2022-04-24 21:39:36 -0600

    Update Optimize For Loop.py

--- Optimize For Loop.py
+++ Optimize For Loop.py
@@ -10,7 +10,6 @@ import numpy as np
 
 
 
-
 '''State Physical Params'''
 r_w = 0.02 #meters
 t_opt = 1e6 #seconds
@@ -20,10 +19,10 @@ var_p = np.zeros([6,1])
 var_p_opt = np.zeros([6,1])
 
 '''Make parameters for the array'''
-L_t = np.arange(0.2,0.301,.01) #meters
-r_0 = np.arange(0.05, 0.2, .001) #meters
+L_t = np.arange(0.2,0.30, 0.01) #meters
+r_0 = np.arange(0.05, 0.2, 0.005) #meters
 P0_gauge = np.arange(70000,200000,10000) #Pascals
-r_g = np.arange(0.002,0.01,.001) #meters
+r_g = np.arange(0.002,0.01,0.001) #meters
 L_r = np.arange(0.1,0.5,0.1) #meters
 r_p = np.arange(0.02,0.04,0.01) #meters
 
@@ -58,13 +57,16 @@ for i in range(np.size(rho_t)):
                             var_p[5] = r_p[o]
                             
                             #Find tie to finish with parameters
-                            t_finish = t_at_finish(var_p, rho_mat, return_only_time=True)
-                            
+                            try:
+                                t_finish = t_at_finish(var_p, rho_mat, return_only_time=True)
+                            except:
+                                import pdb;pdb.set_trace()
                             #Filter results that violate constraints
                             if (2*r_w + 2*r_0[k] < max_height) and (2*r_0[k] < max_width) and (L_r[n]+L_t[j] < max_length) and (r_g[m]/r_w < 1) and t_finish < t_opt:
+                                
                                 #Finish optimal
                                 t_opt = t_finish   
-                                
+                                print(t_opt)
                                 #Optimal parameters
                                 var_p_opt[0] = L_t[j]
                                 var_p_opt[1] = r_0[k]
@@ -73,10 +75,30 @@ for i in range(np.size(rho_t)):
                                 var_p_opt[4] = L_r[n]
                                 var_p_opt[5] = r_p[o]
                                 
-                                rho_mat_opt = rho_mat[i]
-                                
-print(t_opt)
+                                rho_mat_opt = rho_mat
+print('a')
+'''
+xo = var_p_opt 
+print("xo = ", xo)
+res = optimize.minimize(objfun, xo, method='SLSQP', bounds=(db, tb))
+print("Results of constrained optimization:")
+print("Optimal d, t = %g, %g" % tuple(res.x))
+print("Cost of optimized column = ", cost(res.x, P, E, L, rho, ni, nn))
+res = optimize.minimize(objfun, xo, method='Nelder-Mead')
+print("Results of unconstrained optimization:")
+print("Optimal d, t = %g, %g" % tuple(res.x))
+print("Cost of optimized column = ", cost(res.x, P, E, L, rho, ni, nn))
+
+'''
 print(var_p_opt)
+print(t_opt)                                
+                                
+                                
+
+
+
+
+
                                 
                                 
                                 
